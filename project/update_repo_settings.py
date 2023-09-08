7d73c43a5fd408e19bb98a189ed040971ee7b677
Dalghazal <144355287+Dalghazal@users.noreply.github.com>
2023-09-08 09:35:36 +0200

    Update update_repo_settings.py

--- project/update_repo_settings.py
+++ project/update_repo_settings.py
@@ -7,22 +7,50 @@ with open("project/settings.json", "r") as config_file:
     config = json.load(config_file)
 
 # GitHub CLI-Befehl für das Aktualisieren von Repository-Einstellungen
-gh_command = "gh repo set"
+#gh_command = "gh repo set"
 
 # Durchlaufe die Liste der Repository-Konfigurationen und aktualisiere die Einstellungen
 for repo_config in config["repositories"]:
     repo_name = repo_config["name"]
     collaborators = ",".join(repo_config["collaborators"])
     branch_protection = json.dumps(repo_config["branch_protection"])
-    
-    # Konstruiere den GitHub CLI-Befehl
-    command = f"{gh_command} {repo_name} -b '{branch_protection}' --required-approvals {repo_config['required_approvals']} --squash-merge {repo_config['squash_merge']} --collaborators {collaborators}"
 
-    # Führe den Befehl aus
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    #-------------------------------
+    # Konstruiere den GitHub CLI-Befehl für allgemeine Repository-Einstellungen
+    general_settings_command = f"gh repo set {repo_name} --required-approvals {repo_config['required_approvals']} --squash-merge {repo_config['squash_merge']} --collaborators {collaborators}"
 
-    if result.returncode == 0:
+    # Konstruiere den GitHub CLI-Befehl für Branch-Protection-Einstellungen
+    branch_protection_command = f"gh repo branch-protect {repo_name} --pattern '*' --require-code-owners"
+
+    # Führe die Befehle aus
+    result_general = subprocess.run(general_settings_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    result_branch_protection = subprocess.run(branch_protection_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    if result_general.returncode == 0 and result_branch_protection.returncode == 0:
         print(f"Repository {repo_name} aktualisiert.")
     else:
         print(f"Fehler beim Aktualisieren von Repository {repo_name}:")
-        print(result.stderr)
+        print(result_general.stderr)
+        print(result_branch_protection.stderr)
+    
+    
+    
+    
+    
+    
+    
+    #-------------------------------------------------------------
+    
+    # Konstruiere den GitHub CLI-Befehl
+    #command = f"{gh_command} {repo_name} -b '{branch_protection}' --required-approvals {repo_config['required_approvals']} --squash-merge {repo_config['squash_merge']} --collaborators {collaborators}"
+
+    # Führe den Befehl aus
+    #result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    #if result.returncode == 0:
+    #    print(f"Repository {repo_name} aktualisiert.")
+    #else:
+    #    print(f"Fehler beim Aktualisieren von Repository {repo_name}:")
+    #    print(result.stderr)
+    #-----------------------------------------------------------
+
